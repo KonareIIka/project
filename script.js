fc2c72b3e0c7e952c8aa533fec8604f8cecb86d8
Irina <konareika2001@gmail.com>
2022-02-02 01:31:49 +0300

    1.Фильмы

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-"use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
+const personalMovieDB= {
+    count: numberOfFilms,
+    movie: {},
+    actors:{},
+    genres:[],
+    privat: false
+
+};
+const a=prompt('Один из последних просмотренных фильмов?',''),
+    b=prompt('На сколько вы его оцениваете?',''),
+    c=prompt('Один из последних просмотренных фильмов?',''),
+    d=prompt('На сколько вы его оцениваете?','');
+
+personalMovieDB.movie[a]=b;
+personalMovieDB.movie[c]=d;
+console.log(personalMovieDB);
