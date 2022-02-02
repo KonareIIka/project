31fb938c8bb5cd892ca6e3ff610601f3d8289d7a
Irina <konareika2001@gmail.com>
2022-02-03 01:02:08 +0300

    Циклы с фильмами

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ const personalMovieDB= {
     movie: {},
     actors:{},
     genres:[],
-    privat: false
-
-};
-const a=prompt('Один из последних просмотренных фильмов?',''),
-    b=prompt('На сколько вы его оцениваете?',''),
-    c=prompt('Один из последних просмотренных фильмов?',''),
-    d=prompt('На сколько вы его оцениваете?','');
-
-personalMovieDB.movie[a]=b;
-personalMovieDB.movie[c]=d;
+    privat: false};
+for (let i=0;i<numberOfFilms;i++){
+    const a=prompt('Один из последних просмотренных фильмов?',''),
+    b=prompt('На сколько вы его оцениваете?','');
+    if (a.length < 50 && a!='' && b!='' && a!=null & b!=null ) {
+    personalMovieDB.movie[a]=b;
+    console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+  
+}
+if (personalMovieDB.count<10){
+    const c =alert('Просмотренно мало фильмов');
+} else if (personalMovieDB.count>=10 && personalMovieDB.count<30){
+    const c =alert('Вы класический зритель');
+}else if (personalMovieDB.count>=30){
+    const c=alert('Вы киноман')
+} else {
+    const c =alert('Произошла ошибка');
+}
 console.log(personalMovieDB);
+
+ 
